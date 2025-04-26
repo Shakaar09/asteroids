d9b88d547dc9ae167e6277df859810d0bc7b247f
Shakaar09 <deinhard.fd@gmail.com>
2025-04-26 21:02:41 +0200

    C: add many things

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    updateables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+
+    Player.containers = (updateables, drawables)
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
     
 
+
     while True:
-        screen.fill((0, 0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        screen.fill((0, 0, 0))
+        updateables.update(dt)
+        for sprite in drawables:
+            sprite.draw(screen)
 
         print(f"""Starting Asteroids!
 Screen width: {SCREEN_WIDTH}
 Screen height: {SCREEN_HEIGHT}""")
-        
+        dt = clock.tick(60)/1000
         pygame.display.flip()
 
 if __name__ == "__main__":
